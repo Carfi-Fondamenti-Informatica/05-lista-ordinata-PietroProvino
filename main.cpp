f3760f2ff7d470627862be01d1e3b446ddb15353
PietroProvino <127682352+PietroProvino@users.noreply.github.com>
2023-04-30 13:05:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,16 +5,25 @@ int main() {
     for(int i=0;i<10;i++){
         cin >> lista[i];
     }
-    int a=0;
-    int scambio[1];
+    int a=0, b=0;
     cin >> a;
     for(int i=0;i<10;i++){
         for(int j=0; j<i; j++){
             if(lista[i]<lista[j]){
-                scambio[0]=lista[i];
+                b=lista[i];
                 lista[i]=lista[j];
-                lista[j]=scambio[0];
+                lista[j]=b;
             }
+        }
+    }
+    for(int i=0; i<10; i++){
+        if(i==0){
+            b=lista[i];
+            lista[i]=a;
+        }
+        else{
+            lista[i]=b;
+            b=lista[i+1];
         }
     }
     for(int i=0;i<10;i++){
