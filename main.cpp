57750bfccc190a2408eb207b15e198e51e1177d9
PietroProvino <127682352+PietroProvino@users.noreply.github.com>
2023-04-30 12:54:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int lista[10];
+    for(int i=0;i<10;i++){
+        cin >> lista[i];
+    }
+    int a=0;
+    int scambio[1];
+    cin >> a;
+    for(int i=0;i<10;i++){
+        for(int j=0; j<i; j++){
+            if(lista[i]<lista[j]){
+                scambio[0]=lista[i];
+                lista[i]=lista[j];
+                lista[j]=scambio[0];
+            }
+        }
+    }
+    for(int i=0;i<10;i++){
+        cout << lista[i] << " ";
+    }
+    return 0;
 }
